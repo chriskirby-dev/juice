8ad69228c23da8b5627f04e269e5390c925e5ecb
Chris Kirby <chris@chriskirby.me>
2024-05-03 18:30:20 -0400

    Fixed Var type errors

--- Animation/Util.mjs
+++ Animation/Util.mjs
@@ -1,35 +1,33 @@
 class AnimationUtil {
-    
-    static pointDistance( point1, point2 ){
-        
-        const deltas = [];
-        deltas = [ ( point1.x - point2.x ), ( point1.y - point2.y ) ];
-        if( point1.z !== undefined ) deltas.push( point1.z - point2.z ); 
+    static pointDistance(point1, point2) {
+        let deltas = [];
+        deltas = [point1.x - point2.x, point1.y - point2.y];
+        if (point1.z !== undefined) deltas.push(point1.z - point2.z);
 
-        return Math.hypot( ...deltas );
+        return Math.hypot(...deltas);
     }
 
-    static toSeconds(ms){
-        return ms/1000;
+    static toSeconds(ms) {
+        return ms / 1000;
     }
 
-    static diff( a, b ){
+    static diff(a, b) {
         return a - b;
     }
 
-    static deltaMS( last, now ){
-        return ( now - last );
+    static deltaMS(last, now) {
+        return now - last;
     }
 
-    static delta( last, now ){
-        return ( now - last ) / 1000;
+    static delta(last, now) {
+        return (now - last) / 1000;
     }
 
-    static FPS( delta ){
-        return 1/delta;
+    static FPS(delta) {
+        return 1 / delta;
     }
 
-    static degreesToRadians( degrees ){
+    static degreesToRadians(degrees) {
         return degrees * (Math.PI / 180);
     }
 }
